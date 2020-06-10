9e6cf24c937d12e0274f7569da2ae814c6f95dc3
duytvt2410 <duy2410tvt@gmail.com>
2020-06-10 21:42:25 +0700

    ud

--- src/main/java/com/laptrinhjavaweb/config/JPAConfig.java
+++ src/main/java/com/laptrinhjavaweb/config/JPAConfig.java
@@ -64,8 +64,8 @@ public class JPAConfig {
 	Properties addProperties() {
 		Properties properties = new Properties();
 		//properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-		//properties.setProperty("hibernate.hbm2ddl.auto", "create");
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		//properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		//properties.setProperty("hibernate.hbm2dd1.auto", "none");
 		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
 		return properties;
